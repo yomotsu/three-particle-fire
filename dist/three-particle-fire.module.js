1a2c2200921c88cc583828bda613e88583fa7d8e
Vincent Fretin <vincent.fretin@gmail.com>
2022-08-10 15:33:18 +0200

    build

--- dist/three-particle-fire.module.js
+++ dist/three-particle-fire.module.js
@@ -39,7 +39,7 @@ function makeGeometryClass() {
 
 	var Geometry = function Geometry(radius, height, particleCount) {
 
-		THREE.BufferGeometry.call(this);
+		var geometry = new THREE.BufferGeometry();
 
 		var halfHeight = height * 0.5;
 		var position = new Float32Array(particleCount * 3);
@@ -65,12 +65,11 @@ function makeGeometryClass() {
 			}
 		}
 
-		this.addAttribute('position', new THREE.BufferAttribute(position, 3));
-		this.addAttribute('randam', new THREE.BufferAttribute(randam, 1));
-		this.addAttribute('sprite', new THREE.BufferAttribute(sprite, 1));
+		geometry.setAttribute('position', new THREE.BufferAttribute(position, 3));
+		geometry.setAttribute('randam', new THREE.BufferAttribute(randam, 1));
+		geometry.setAttribute('sprite', new THREE.BufferAttribute(sprite, 1));
+		return geometry;
 	};
-
-	Geometry.prototype = Object.create(THREE.BufferGeometry.prototype);
 
 	return Geometry;
 }
@@ -108,18 +107,18 @@ function makeMaterialClass() {
 		var uniforms = {
 			color: { type: "c", value: null },
 			size: { type: "f", value: 0.0 },
-			texture: { type: "t", value: getTexture() },
+			map: { type: "t", value: getTexture() },
 			time: { type: "f", value: 0.0 },
 			heightOfNearPlane: { type: "f", value: 0.0 }
 		};
 
-		THREE.ShaderMaterial.call(this, {
+		var material = new THREE.ShaderMaterial({
 
 			uniforms: uniforms,
 
 			vertexShader: ['attribute float randam;', 'attribute float sprite;', 'uniform float time;', 'uniform float size;', 'uniform float heightOfNearPlane;', 'varying float vSprite;', 'varying float vOpacity;', 'float PI = 3.14;', 'float quadraticIn( float t ) {', 'float tt = t * t;', 'return tt * tt;', '}', 'void main() {', 'float progress = fract( time + ( 2.0 * randam - 1.0 ) );', 'float progressNeg = 1.0 - progress;', 'float ease = quadraticIn( progress );', 'float influence = sin( PI * ease );', 'vec3 newPosition = position * vec3( 1.0, ease, 1.0 );', 'gl_Position = projectionMatrix * modelViewMatrix * vec4( newPosition, 1.0 );', 'gl_PointSize = ( heightOfNearPlane * size ) / gl_Position.w;', 'vOpacity = min( influence * 4.0, 1.0 ) * progressNeg;', 'vSprite = sprite;', '}'].join('\n'),
 
-			fragmentShader: ['uniform vec3 color;', 'uniform sampler2D texture;', 'varying float vSprite;', 'varying float vOpacity;', 'void main() {', 'vec2 texCoord = vec2(', 'gl_PointCoord.x * ' + ONE_SPRITE_ROW_LENGTH + ' + vSprite,', 'gl_PointCoord.y', ');', 'gl_FragColor = vec4( texture2D( texture, vec2( texCoord ) ).xyz * color * vOpacity, 1.0 );', '}'].join('\n'),
+			fragmentShader: ['uniform vec3 color;', 'uniform sampler2D map;', 'varying float vSprite;', 'varying float vOpacity;', 'void main() {', 'vec2 texCoord = vec2(', 'gl_PointCoord.x * ' + ONE_SPRITE_ROW_LENGTH + ' + vSprite,', 'gl_PointCoord.y', ');', 'gl_FragColor = vec4( texture2D( map, texCoord ).xyz * color * vOpacity, 1.0 );', '}'].join('\n'),
 
 			blending: THREE.AdditiveBlending,
 			depthTest: true,
@@ -129,24 +128,27 @@ function makeMaterialClass() {
 
 		});
 
-		this.color = new THREE.Color(0xff2200);
-		this.size = 0.4;
-		this.setValues(parameters);
+		material.color = new THREE.Color(0xff2200);
+		material.size = 0.4;
 
-		this.uniforms.color.value = this.color;
-		this.uniforms.size.value = this.size;
-	};
+		if (parameters !== undefined) {
+			material.setValues(parameters);
+		}
 
-	Material.prototype = Object.create(THREE.ShaderMaterial.prototype);
+		material.uniforms.color.value = material.color;
+		material.uniforms.size.value = material.size;
 
-	Material.prototype.update = function (delta) {
+		material.update = function (delta) {
 
-		this.uniforms.time.value = (this.uniforms.time.value + delta) % 1;
-	};
+			material.uniforms.time.value = (material.uniforms.time.value + delta) % 1;
+		};
 
-	Material.prototype.setPerspective = function (fov, height) {
+		material.setPerspective = function (fov, height) {
 
-		this.uniforms.heightOfNearPlane.value = Math.abs(height / (2 * Math.tan(THREE.Math.degToRad(fov * 0.5))));
+			material.uniforms.heightOfNearPlane.value = Math.abs(height / (2 * Math.tan(THREE.MathUtils.degToRad(fov * 0.5))));
+		};
+
+		return material;
 	};
 
 	return Material;
